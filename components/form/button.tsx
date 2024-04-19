0ff1f72dc2c7e15b1979decb331affb5b7719208
shin <sinej1020@gmail.com>
2024-04-19 15:26:09 +0900

    폼 버튼 추가

--- components/form/button.tsx
+++ components/form/button.tsx
@@ -11,7 +11,9 @@ const FormButton = (props: Props) => {
     return (
         <>
             <button disabled={loading}
-                    className="primary-btn bg-blue030 hover:bg-blue040 transition-color s14-regular-lh20 flex items-center justify-center"
+                    className="primary-btn bg-blue030 hover:bg-blue040 transition-color s14-regular-lh20
+                    flex items-center justify-center
+                     disabled:bg-gray030 disabled:text-gray060 cursor-pointer disabled:cursor-default"
             >
                 {loading ?
                     <CgSpinner className="animate-spin" size={20} /> :
