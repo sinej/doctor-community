1bfde8a4a4af762a81024663c53ccecf07fa5f8f
shin <sinej1020@gmail.com>
2024-04-21 21:25:38 +0900

    vitest 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from "vite-plugin-eslint";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint({ exclude: ['/virtual: /**', 'node_modules/**'] })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './app/utils/test/setupTests.js'
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, 'src')  }]
+  }
 })
