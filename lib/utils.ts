6ccc4b3b3dd295026a8eac57e570a4945f8a9c7c
shin <sinej1020@gmail.com>
2024-04-28 23:16:05 +0900

    cn 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export function formatToTimeAgo(date: string): string {
   const dayInMs = 1000 * 60 * 60 * 24;
   const time = new Date(date).getTime();
@@ -7,4 +10,9 @@ export function formatToTimeAgo(date: string): string {
   const formatter = new Intl.RelativeTimeFormat("ko");
 
   return formatter.format(diff, "days");
+}
+
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
